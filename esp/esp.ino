756f95cd4791abe73fbbb4b8a80a829cf5d40429
nguyenduyhuy-art <80386874+nguyenduyhuy-art@users.noreply.github.com>
2021-03-11 22:41:17 +0700

    Create esp.ino

---
+++ esp/esp.ino
@@ -0,0 +1,289 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <Ticker.h>
+#include <BlynkSimpleEsp8266.h>
+
+WiFiUDP ntpUDP;                 //giao thưc TCP
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+/*NTPClient n(u,"1.asia.pool.ntp.org",7*3600);// khai báo truy cập vào NTP sever khu vực ASIA, mui +7, 60*60= 3600s */
+//Week Days
+String weekDays[7] = {"0", "1", "2", "3", "4", "5", "6"};
+
+//Month names
+String months[12] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+
+
+char auth[] = "qJGUs1-T3mqFFpvshJwGSXHvXdGrRTQ5";
+String myblynk, tam, temperature = "11", humidity = "34", light = "0", j;
+
+String date;
+
+#define N_COMMAND 5
+class Command {
+  public:
+    const String cmd[N_COMMAND] = {"scan", "connect", "disconnect", "info", "get"};
+    const uint8_t ncmd = N_COMMAND;
+    bool done = false;
+    String buffer = "";
+    String param;
+
+    int read() {
+      while (Serial.available() > 0) {
+        char b = Serial.read();
+        if (b == '\n') return -1;
+        if (b == ';') {
+          String temp = buffer;
+          buffer = "";
+          int dot = temp.indexOf('.');
+          if (dot != -1) {
+            String tempcmd = temp.substring(0, dot);
+            for (int i = 0; i < ncmd; i++) {
+              if (tempcmd == cmd[i]) {
+                if (dot + 1 < temp.length()) param = temp.substring(dot + 1);
+                return i;
+              }
+            }
+          } else {
+            for (int i = 0; i < ncmd; i++) {
+              if (temp == cmd[i]) {
+                return i;
+              }
+            }
+          }
+        } else {
+          buffer += String(b);
+        }
+      }
+      return -1;
+    }
+};
+Command cmd;
+int strength;
+
+class {
+  public:
+    int order[10];
+    int n;
+    String password;
+    bool CONNECTED = false;
+
+    void sort(int nn) {
+      int count = 0;
+      for (int i = 0; i < nn; i++) {
+        if (WiFi.RSSI(i) > -70) {
+          int tt = count;
+          order[tt] = i;
+          if (tt != 0) {
+            while (WiFi.RSSI(order[tt]) > WiFi.RSSI(order[tt - 1])) {
+              swp(tt, tt - 1);
+              tt--;
+              if (tt == 0) break;
+            }
+          }
+          count ++;
+        }
+      }
+      n = count;
+    }
+
+    void swp(int i, int j) {
+      int temp = order[i];
+      order[i] = order[j];
+      order[j] = temp;
+    }
+
+} wf;
+/////////////////////////////////////////////////DOC THOI GIAN THUC TU SEVER//////////////////////////////////////////////////////
+Ticker ticker1;
+void sentdata() {
+  unsigned long epochTime = timeClient.getEpochTime();
+  int currentHour = timeClient.getHours();
+  int currentMinute = timeClient.getMinutes();
+  String weekDay = weekDays[timeClient.getDay()];
+
+  //Get a time structure
+  struct tm *ptm = gmtime ((time_t *)&epochTime);
+  int monthDay = ptm->tm_mday;
+  int currentMonth = ptm->tm_mon + 1;
+  int currentYear = ptm->tm_year + 1900;
+
+  //Print complete date:
+  String currentDate = "time." + String(currentMinute) + ":" + String(currentHour) + "," +  String(weekDay) + "," + String(monthDay) + "/" + String(currentMonth) + "/" + String(currentYear) + ";"  ;
+  if (WiFi.isConnected()) Serial.print(String("strength.") + WiFi.RSSI() + ";");
+  Serial.print(currentDate);
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////DOC TU APP BLYNK XUONG////////////////////////////////////////////////////////////////////////
+
+BlynkTimer timer;
+BLYNK_WRITE(V3) {   //Cai dat gia tri Nhiet do//
+  int pinValue = param.asInt();
+  Serial.print(String("setpoint.") + String("t:") +  pinValue + String(";"));
+}
+BLYNK_WRITE(V4) {     //Cai dat gia tri Do am//
+  int pinValue = param.asInt();
+  Serial.print(String("setpoint.") +  String("h:") + pinValue + String(";"));
+}
+BLYNK_WRITE(V5) {      // Cai dat gia tri Anh sang//
+  int pinValue = param.asInt();
+  Serial.print(String("setpoint.") +  String("as:") + pinValue + String(";"));
+}
+
+//VIET LEN APP BLYNK//////
+void myTimerEvent()
+{
+  Blynk.virtualWrite(V0, temperature);
+  Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V2, light);
+  timeClient.update(); // cập nhật sever thời gian hiện tại
+
+  //  Serial.print(timeClient.getHours());
+  //  Serial.print(":");
+  //  Serial.print(String(timeClient.getMinutes()));
+  //  Serial.print(":");
+  //  Serial.print(String(timeClient.getSeconds()));
+  //  Serial.println("");
+  //  Serial.print(String(timeClient.getDay()));
+  //  Serial.println("/");
+  //  delay(1000);
+  //    Blynk.run();
+  //    timer.run();
+}
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(100);
+  Serial.print("flush;");
+  // ket noi wifi
+  ticker1.attach(1, sentdata);  // 1s thực hiện void sentdata
+  cmd.buffer = "";
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(25200);
+  //  Serial.println(WiFi.SSID() + ":" + WiFi.psk());
+  //  unsigned long t1 = millis();
+  //  while (WiFi.status() != WL_CONNECTED) {
+  //    if (millis() - t1 > 3000) break;
+  //  }
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop() {
+  if (WiFi.isConnected()) {
+    if (!wf.CONNECTED) {
+      wf.CONNECTED = true;
+      Serial.print("connected." + WiFi.SSID() + "::" + WiFi.psk() + ";");
+    }
+    if (!Blynk.connected()) {
+      char c_ssid[50];
+      char c_pw[30];
+      WiFi.SSID().toCharArray(c_ssid, WiFi.SSID().length());
+      WiFi.psk().toCharArray(c_pw, WiFi.psk().length());
+      Blynk.begin(auth, c_ssid, c_pw);
+    }
+  }
+  timeClient.update();
+  if (wf.CONNECTED) {
+    timeClient.update(); // cập nhật sever thời gian hiện tại
+    /* Serial.print(n.getHours());
+      Serial.print(":");
+      inso(n.getMinutes());
+      Serial.print(":");
+      inso(n.getSeconds());
+      Serial.println(""); */
+    Blynk.run();
+    timer.run();
+  }
+  //read cmd
+  switch (cmd.read()) {
+    case 0:  // scan
+      { scan();
+        break;
+      }
+    case 1:  // connect
+      //  <ssid>::<password>
+      { int sp = cmd.param.indexOf("::");
+        String ssid = cmd.param.substring(0, sp);
+        String pw = cmd.param.substring(sp + 2);
+        WiFi.disconnect();
+        delay(300);
+        WiFi.begin(ssid, pw);
+        unsigned long t = millis();
+        while (WiFi.status() != WL_CONNECTED)
+        {
+          delay(100);
+        }
+        char c_ssid[50];
+        char c_pw[30];
+        ssid.toCharArray(c_ssid, ssid.length());
+        pw.toCharArray(c_pw, pw.length());
+        Blynk.begin(auth, c_ssid, c_pw);
+        timer.setInterval(1000L, myTimerEvent); //1s gui du lieu 1 lan len Blynk
+        timeClient.begin();// khởi động truy cập sever thời gian
+        Serial.print("status.1;");
+        wf.CONNECTED = true;
+        break;
+      }
+    case 2:
+      {
+        WiFi.disconnect();
+        delay(100);
+        Serial.print("status.0;");
+      }
+      break;
+    case 3:  // info.<t>,<h>,<l>;
+      {
+        String s = cmd.param; // <t>,<h>,<l>   "24.5,96,0"
+        int vt_phay1 = s.indexOf(',');                   //vi tri cua ky tu =4
+        int vt_phay2 = s.indexOf(',', vt_phay1 + 1);
+        temperature = s.substring(0, vt_phay1);          //chuoi tu vi tri 0 - (vt_phay1 -1) = 24.5
+        humidity = s.substring(vt_phay1, vt_phay2);
+        light = s.substring(vt_phay2, s.length());
+      }
+      break;
+    case 4:
+      {
+        String s = cmd.param;
+        if (s == "wifi") {
+          if (WiFi.isConnected()) {
+            Serial.print("connected." + WiFi.SSID() + "::" + WiFi.psk() + ";");
+          }
+        }
+      }
+  }
+
+}
+
+
+
+
+//  list.<n>,<ssid1>::<strength1>\\<ssid2>....;
+void scan() {
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  //  WiFi.mode(WIFI_STA);
+  //  WiFi.disconnect();
+  //  delay(100);
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  wf.sort(n);
+  Serial.print("list.");
+  for (int i = 0; i < wf.n; i++) {
+    // Print SSID and RSSI for each network found
+    if (i > 0) Serial.print("\\\\");  //name
+    Serial.print(WiFi.SSID(wf.order[i]));  //name
+    Serial.print("::");
+    Serial.print(WiFi.RSSI(wf.order[i]));  //strength in dBm
+  }
+  Serial.print(';');  //name
+}
+
+/*void inso(byte so){
+  if(so < 10)
+   Serial.print('0');
+  Serial.print(so, DEC);
+  }*/
